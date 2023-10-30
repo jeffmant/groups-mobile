fe2b8594402fff4b504260512da78a1ac7b7f117
Jefferson Mantovani <jefferson.mantovani@bolttech.io>
2023-10-30 14:21:12 -0300

    fix: status bar style

--- App.tsx
+++ App.tsx
@@ -3,12 +3,18 @@ import { Groups } from '@screens/Groups';
 import theme from './src/theme';
 import { useFonts, Roboto_400Regular, Roboto_700Bold } from '@expo-google-fonts/roboto'
 import { Loading } from '@components/Loading';
+import { StatusBar } from 'react-native';
 
 export default function App() {
   const [fontsLeaded, fontsError] = useFonts({ Roboto_400Regular, Roboto_700Bold })
 
   return (
     <ThemeProvider theme={theme}>
+      <StatusBar
+        barStyle={'light-content'}
+        backgroundColor={'transparent'}
+        translucent
+      />
       { !fontsLeaded ? <Groups /> : <Loading />}
     </ThemeProvider>
   );
