7714f1dcc92441ce715c477f3d6b46990352af28
Jefferson Mantovani <jefferson.mantovani@bolttech.io>
2023-10-30 15:57:00 -0300

    feat: create component for empty lists

--- src/screens/Groups/index.tsx
+++ src/screens/Groups/index.tsx
@@ -2,14 +2,32 @@ import { Header } from '@components/Header';
 import { Container } from './styles';
 import { Highlight } from '@components/Highlight';
 import { GroupCard } from '@components/GroupCard';
+import { useState } from 'react';
+import { FlatList } from 'react-native';
+import { ListEmpty } from '@components/ListEmpty';
 
 export function Groups() {
+  const [groups, setGroups] = useState<string[]>([])
+
   return (
     <Container>
       <Header />
+
       <Highlight title='Title' subtitle='Subtitle' />
       
-      <GroupCard title='Grupo 1' />
+      <FlatList
+        data={groups}
+        keyExtractor={item => item}
+        contentContainerStyle={groups?.length === 0 && { flex: 1 }}
+        renderItem={({ item }) => (
+          <GroupCard title={item} />
+        )}
+        ListEmptyComponent={() => 
+          <ListEmpty 
+            message='Nenhum grupo cadastrado' 
+          />
+        }
+      />
 
     </Container>
   );
