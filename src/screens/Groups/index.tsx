3ff9b54f4a1c051285575b9c36f16a2c2e7719dc
Jefferson Mantovani <jefferson.mantovani@bolttech.io>
2023-10-30 13:35:20 -0300

    feat: config styled component as theme

--- src/screens/Groups/index.tsx
+++ src/screens/Groups/index.tsx
@@ -1,9 +1,8 @@
-import { Container, Title } from './Groups.styles';
+import { Container } from './styles';
 
 export function Groups() {
   return (
     <Container>
-      <Title>Groups</Title>
     </Container>
   );
 }
