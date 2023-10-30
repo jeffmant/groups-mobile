aa39a6fb487aa0a550b0a4b91b41708f33345699
Jefferson Mantovani <jefferson.mantovani@bolttech.io>
2023-10-30 16:37:47 -0300

    feat: create input component

--- src/components/NewGroup/index.tsx
+++ src/components/NewGroup/index.tsx
@@ -1,6 +1,8 @@
 import { Header } from "@components/Header";
 import { Container, Content, Icon } from "./styles";
 import { Highlight } from "@components/Highlight";
+import { Input } from "@components/Input";
+import { Button } from "@components/Button";
 
 export function NewGroup () {
   return (
@@ -11,6 +13,10 @@ export function NewGroup () {
         <Icon />
 
         <Highlight title="Novo Grupo" subtitle="Crie um grupo para adicionar pessoas" />
+
+        <Input placeholder="Digite o nome do grupo" style={{ marginBottom: 16 }} />
+
+        <Button title="Criar" />
       </Content>
     </Container>
   )
