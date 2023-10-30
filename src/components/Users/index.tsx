ae71cc5c397802dde49197c1b906b4fff1b37462
Jefferson Mantovani <jefferson.mantovani@bolttech.io>
2023-10-30 17:42:16 -0300

    feat: create header filter

--- src/components/Users/index.tsx
+++ src/components/Users/index.tsx
@@ -1,11 +1,17 @@
 import { Header } from "@components/Header";
-import { Container, Form } from "./styles";
+import { Container, Form, HeaderList, UsersCount } from "./styles";
 import { Highlight } from "@components/Highlight";
 import { ButtonIcon } from "@components/ButtonIcon";
 import { Input } from "@components/Input";
 import { Filter } from "@components/Filter";
+import { FlatList } from "react-native";
+import { useState } from "react";
+
 
 export function Users () {
+  const [groups, setGroups] = useState(['Grupo A', 'Grupo B'])
+  const [seletedGroup, setSelectedGroup] = useState(groups[0])
+  
   return (
     <Container>
       <Header showBackButton />
@@ -17,10 +23,25 @@ export function Users () {
         <ButtonIcon icon="add" />
       </Form>
 
-      <Filter 
-        title="Time A"
-        isActive={true}
-      />
+      <HeaderList>
+        <FlatList
+          data={['Grupo A', 'Grupo B']}
+          keyExtractor={item => item}
+          renderItem={({ item }) => (
+            <Filter 
+              title={item}
+              isActive={item === seletedGroup}
+              onPress={() => setSelectedGroup(item)}
+            />
+          )}
+          horizontal={true}
+        />
+
+        <UsersCount>
+          {groups?.length}
+        </UsersCount>
+      </HeaderList>
+
     </Container>
   );
 };
