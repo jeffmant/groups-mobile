ace1b19ae9180e54578ab9c2b1b1be19459876ae
Jefferson Mantovani <jefferson.mantovani@bolttech.io>
2023-10-30 15:44:43 -0300

    feat: create group card component

--- src/screens/Groups/index.tsx
+++ src/screens/Groups/index.tsx
@@ -1,10 +1,16 @@
 import { Header } from '@components/Header';
 import { Container } from './styles';
+import { Highlight } from '@components/Highlight';
+import { GroupCard } from '@components/GroupCard';
 
 export function Groups() {
   return (
     <Container>
       <Header />
+      <Highlight title='Title' subtitle='Subtitle' />
+      
+      <GroupCard title='Grupo 1' />
+
     </Container>
   );
 }
