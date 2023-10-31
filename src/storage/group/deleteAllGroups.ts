4a038f9f2ff24c3c37b666b3ebb493aa448d8b4b
Jefferson Mantovani <jefferson.mantovani@bolttech.io>
2023-10-31 17:00:09 -0300

    refactor: delete users when delete group

--- src/storage/group/deleteAllGroups.ts
+++ src/storage/group/deleteAllGroups.ts
@@ -1,8 +1,17 @@
 import AsyncStorage from "@react-native-async-storage/async-storage";
 import { GROUP_COLLECTION } from "@storage/storage.config";
+import { deleteAllUsersByGroup } from "@storage/user/deleteAllUsersByGroup";
 
 export async function deleteAllGroups (): Promise<void> {
   try {
+    const storedGroups = await AsyncStorage.getItem(GROUP_COLLECTION)
+
+    if (storedGroups) {
+      for (const group of JSON.parse(storedGroups)) {
+        await deleteAllUsersByGroup(group)
+      }
+    }
+
     await AsyncStorage.setItem(GROUP_COLLECTION, JSON.stringify([]))
   } catch (error) {
     throw error
--- src/storage/group/deleteGroupByName.ts
+++ src/storage/group/deleteGroupByName.ts
@@ -1,12 +1,17 @@
 import AsyncStorage from "@react-native-async-storage/async-storage";
 import { GROUP_COLLECTION } from "@storage/storage.config";
 import { getAllGroups } from "./getAllGroups";
+import { deleteAllUsersByGroup } from "@storage/user/deleteAllUsersByGroup";
 
 export async function deleteGroupByName (groupName: string): Promise<void> {
   try {
     const storedGroups = await getAllGroups()
-    const filteredFroups = storedGroups.filter((group) => group !== groupName)
-    await AsyncStorage.setItem(GROUP_COLLECTION, JSON.stringify(filteredFroups))
+
+    const filteredGroups = storedGroups.filter((group) => group !== groupName)
+
+    await deleteAllUsersByGroup(groupName)
+    
+    await AsyncStorage.setItem(GROUP_COLLECTION, JSON.stringify(filteredGroups))
   } catch (error) {
     throw error
   }
